3b1f0a0a566d201cdf5b8b5128ea0c0b79a4ffd7
Jon Warbrick <jw35@cam.ac.uk>
2018-09-20 17:34:53 +0100

    merge.py aparently working

--- merge.py
+++ merge.py
@@ -41,11 +41,17 @@ def load_journeys(day):
     return journeys
 
 
-def do_merge(trips, journeys):
+def do_merge(trip_data, journey_data):
+    '''
+    Merge trips and journeys into one list, matching those with
+    identical departure time, origin and destination
+    '''
 
+    # Group trips by OriginAimedDepartureTime/OriginRef/DestinationRef
+    # and create a sorted list of keys
     trip_index = collections.defaultdict(list)
     trip_list = []
-    for trip in trips['trips']:
+    for trip in trip_data['trips']:
         key = (
             trip['OriginAimedDepartureTime'][:19],
             trip['OriginRef'],
@@ -53,11 +59,12 @@ def do_merge(trips, journeys):
         )
         trip_index[key].append(trip)
     trip_list = sorted(trip_index.keys())
-    logger.debug(json.dumps(trip_list, indent=4))
+    logger.info('Grouped %s trips into %s groups', len(trip_data['trips']), len(trip_list))
 
+    # Dito for journeys, grouped by DepartureTime and first and last stop
     journey_index = collections.defaultdict(list)
     journey_list = []
-    for journey in journeys['journeys']:
+    for journey in journey_data['journeys']:
         key = (
             journey['DepartureTime'],
             journey['stops'][0]['StopPointRef'],
@@ -65,26 +72,31 @@ def do_merge(trips, journeys):
         )
         journey_index[key].append(journey)
     journey_list = sorted(journey_index.keys())
-    logger.debug(json.dumps(journey_list, indent=4))
+    logger.info('Grouped %s journeys into %s groups', len(journey_data['journeys']), len(journey_list))
 
+    # Merge trips and journeys into one list that has one row per distinct
+    # departure tile/origin/destination and whose first column contains a
+    # (possibly empty) list of trips and whose second column contains a
+    # (possibly empty) list of journeys
     results = []
     while trip_list and journey_list:
         if trip_list[0] < journey_list[0]:
             results.append({
+                'key': trip_list[0],
                 'trips': trip_index[trip_list.pop(0)],
                 'journeys': []
             })
         elif trip_list[0] > journey_list[0]:
             results.append({
+                'key': journey_list[0],
                 'trips': [],
                 'journeys': journey_index[journey_list.pop(0)]
             })
         else:
-            trips = trip_index[trip_list.pop(0)]
-            journeys = journey_index[journey_list.pop(0)]
             results.append({
-                'trips': trips,
-                'journeys': journeys
+                'key': trip_list[0],
+                'trips': trip_index[trip_list.pop(0)],
+                'journeys': journey_index[journey_list.pop(0)]
             })
     while trip_list:
         results.append({
@@ -97,70 +109,154 @@ def do_merge(trips, journeys):
             'journeys': journey_index[journey_list.pop(0)]
         })
 
+    logger.info('Created %s merged rows', len(results))
+
     return results
 
 
-def sumarise(results):
+seps = {
+    '0-1': (' ', ' ', '\u21a6'),
+    '1-0': (' ', ' ', '\u21a4'),
+    '*-*': ('\u2533', '\u2503', '\u253b'),
+    '0-*': ('\u250f', '\u2503', '\u2517'),
+    '1-*': ('\u2533', '\u2503', '\u2517'),
+    '*-0': ('\u2513', '\u2503', '\u251b'),
+    '*-1': ('\u2533', '\u2503', '\u251b'),
+}
 
-    output = csv.writer(sys.stdout)
 
-    for result in results:
+def sumarise(day, results):
 
-        if len(result['journeys']) == 0:
-            for trip in result['trips']:
-                row = (
-                    'Unmatched trip',
+    filename = 'merged-{:%Y-%m-%d}.csv'.format(day)
+    logger.info('Outputing CSV to %s', filename)
+
+    with open(filename, 'w', newline='') as csvfile:
+
+        output = csv.writer(csvfile)
+
+        output.writerow((
+            'Type',
+            '',
+            'Departure time',
+            'From',
+            'To',
+            '',
+            'Trip-Line',
+            'Trip-Operator',
+            'Trip-Direction',
+            'Trip-Vehicle',
+            'Trip-Positions',
+            '',
+            '',
+            '',
+            'Journey-Line',
+            'Journey-Operator',
+            'Journey-Direction',
+        ))
+
+        # For each result row
+        for result in results:
+
+            # Get the key, and *copies* of the trip row(s) and the journey row(s)
+            key = result['key']
+            trips = result['trips'][:]
+            journeys = result['journeys'][:]
+
+            # Derive the type string
+            type = ((str(len(trips)) if len(trips) <= 1 else '*') +
+                    '-' +
+                    (str(len(journeys)) if len(journeys) <= 1 else '*'))
+            logger.debug('tlen %s, jlen %s, type %s', len(trips), len(journeys), type)
+
+            # Pre-populate a list of separators
+            rows = max(len(trips), len(journeys))
+            if type in seps:
+                seperator = [seps[type][1] for _ in range(rows)]
+                seperator[0] = seps[type][0]
+                seperator[-1] = seps[type][2]
+            else:
+                seperator = [' ' for _ in range(rows)]
+
+            row = 0
+            # Process matching trips/journeys
+            while trips and journeys:
+                r = (
+                    type,
                     '',
-                    trip['OriginAimedDepartureTime'][:19],
-                    trip['OriginRef'],
-                    trip['OriginName'],
-                    trip['DestinationRef'],
-                    trip['DestinationName'],
+                    key[0],
+                    key[1],
+                    key[2],
                     '',
-                    trip['PublishedLineName'],
+                ) + trip_fields(trips.pop(0)) + ('', seperator[row], '') + journey_fields(journeys.pop(0))
+                output.writerow(r)
+                row += 1
+            # Process remaining trips
+            while trips:
+                r = (
+                    type,
                     '',
+                    key[0],
+                    key[1],
+                    key[2],
                     '',
-                )
-                output.writerow(row)
-        elif len(result['trips']) == 0:
-            for journey in result['journeys']:
-                row = (
-                    'Unmatched journey',
+                ) + trip_fields(trips.pop(0)) + ('', seperator[row], '') + journey_fields(None)
+                output.writerow(r)
+                row += 1
+            # Process remaining journeys
+            while journeys:
+                r = (
+                    type,
                     '',
-                    journey['DepartureTime'],
-                    journey['stops'][0]['StopPointRef'],
-                    journey['stops'][0]['CommonName'],
-                    journey['stops'][-1]['StopPointRef'],
-                    journey['stops'][-1]['CommonName'],
+                    key[0],
+                    key[1],
+                    key[2],
                     '',
-                    ''
-                    '',
-                    journey['Service']['LineName'],
-                )
-                output.writerow(row)
-        elif len(result['journeys']) == 1 and len(result['trips']) == 1:
-            trip = result['trips'][0]
-            journey = result['journeys'][0]
-            row = (
-                'Matched trip/journey',
-                '',
-                trip['OriginAimedDepartureTime'][:19],
-                trip['OriginRef'],
-                trip['OriginName'],
-                trip['DestinationRef'],
-                trip['DestinationName'],
-                '',
-                trip['PublishedLineName'],
-                '',
-                journey['Service']['LineName'],
-            )
-            output.writerow(row)
-        else:
-            row = (
-                'Multi-matched journey(s)',
-                '',
-            )
-            output.writerow(row)
+                ) + trip_fields(None) + ('', seperator[row], '') + journey_fields(journeys.pop(0))
+                output.writerow(r)
+                row += 1
+
+    logger.info('Output done')
+
+
+def trip_fields(trip):
+    if trip is None:
+        return ('', '', '', '', '')
+    return (
+        trip['LineRef'],
+        trip['OperatorRef'],
+        trip['DirectionRef'],
+        trip['VehicleRef'],
+        len(trip['positions']),
+    )
+
+
+def journey_fields(journey):
+    if journey is None:
+        return ('', '', '')
+    return (
+        journey['Service']['LineName'],
+        journey['Service']['OperatorCode'],
+        journey['Direction']
+    )
+
+
+def emit_merged(day, bounding_box, results):
+    '''
+    Print merged details in json to 'merged-<YYYY>-<mm>-<dd>.json'
+    '''
+
+    filename = 'merged-{:%Y-%m-%d}.json'.format(day)
+    logger.info('Outputing JSON to %s', filename)
+
+    with open(filename, 'w', newline='') as jsonfile:
+        output = {
+            'day': day.strftime('%Y-%m-%d'),
+            'bounding_box': bounding_box,
+            'merged': results
+        }
+        json.dump(results, jsonfile, indent=4, sort_keys=True)
+
+    logger.info('Output done')
 
 
 def main():
@@ -173,22 +269,24 @@ def main():
         logger.error('Failed to parse date')
         sys.exit()
 
-    trips = load_trips(day)
-    journeys = load_journeys(day)
+    trip_data = load_trips(day)
+    journey_data = load_journeys(day)
 
-    if trips['day'] != journeys['day']:
+    if trip_data['day'] != journey_data['day']:
         logger.error('Date in trips (%s) doesn\'t match that in journeys (%s)',
-                     trips['day'], journeys['day'])
+                     trip_data['day'], journey_data['day'])
         sys.exit()
 
-    if trips['bounding_box'] != journeys['bounding_box']:
+    if trip_data['bounding_box'] != journey_data['bounding_box']:
         logger.error('Bounding box in trips (%s) doesn\'t match that in journeys (%s)',
-                     trips['bounding_box'], journeys['bounding_box'])
+                     trip_data['bounding_box'], journey_data['bounding_box'])
         sys.exit()
 
-    results = do_merge(trips, journeys)
+    results = do_merge(trip_data, journey_data)
 
-    sumarise(results)
+    emit_merged(day, trip_data['bounding_box'], results)
+
+    sumarise(day, results)
 
     logger.info('Stop')
 
