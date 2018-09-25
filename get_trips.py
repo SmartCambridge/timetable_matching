7f03a4f8dfeed6c7fc9999655073b693985a444f
Jon Warbrick <jw35@cam.ac.uk>
2018-09-25 17:43:37 +0100

    Calculate departure and arrival times

--- get_trips.py
+++ get_trips.py
@@ -50,12 +50,13 @@ import logging
 import os
 import sys
 
+from haversine import haversine
+
 from util import (
     API_SCHEMA, BOUNDING_BOX, LOAD_PATH, get_client, get_stops,
     update_bbox, lookup
 )
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 logger = logging.getLogger('__name__')
 
 other_stops = {}
@@ -149,6 +150,78 @@ def get_trips(client, schema, date, interesting_stops):
     return list(trips.values())
 
 
+def derive_timings(trips):
+    '''
+    Workout actual departure and arrival timings for each trip
+    '''
+
+    logger.info('Deriving timings for %s trips', len(trips))
+
+    threshold = 100
+
+    for trip in trips:
+
+        logger.debug('')
+        logger.debug('Processing %s to %s at %s', trip['OriginRef'],
+                     trip['DestinationRef'], trip['OriginAimedDepartureTime'])
+
+        origin = (float(trip['OriginStop']['latitude']),
+                  float(trip['OriginStop']['longitude']))
+        destination = (float(trip['DestinationStop']['latitude']),
+                       float(trip['DestinationStop']['longitude']))
+
+        departure_state = 'before'
+        arrival_state = 'before'
+        departure_position = arrival_position = None
+
+        for row, position in enumerate(trip['positions']):
+
+            logger.debug('')
+            logger.debug('Processing position %s', row)
+            logger.debug('initial origin state %s; destination state %s', departure_state, arrival_state)
+
+            here = (float(position['Latitude']), float(position['Longitude']))
+
+            origin_distance = haversine(here, origin) * 1000  # in meters
+
+            logger.debug('Origin distance %s', origin_distance)
+
+            if departure_state == 'before' and origin_distance < threshold:
+                departure_state = 'at'
+                logger.debug('Departure state transition before --> at')
+            elif departure_state == 'at' and origin_distance > threshold:
+                departure_position = row - 1
+                departure_state = 'after'
+                logger.debug('Departure state transition at --> after')
+
+            destination_distance = haversine(here, destination) * 1000  # in meters
+
+            logger.debug('Destination distance %s', destination_distance)
+
+            if arrival_state == 'before' and destination_distance < threshold:
+                arrival_state = 'at'
+                arrival_position = row
+                logger.debug('Arrival state transition before --> at')
+
+            logger.debug('Final origin state %s; destination state %s', departure_state, arrival_state)
+
+        # Try a bit harder if we still don't have an arrival_position - use
+        # the very last position if it's within threshold * 4
+        if arrival_position is None:
+            final_position = trip['positions'][-1]
+            final = (float(final_position['Latitude']), float(final_position['Longitude']))
+            if (haversine(final, destination) * 1000) < (threshold * 4):
+                arrival_position = len(trip['positions']) - 1
+                logger.debug('Using final position for arrival')
+
+
+        logger.debug('Departure row %s; arrival row %s', departure_position, arrival_position)
+        logger.debug('')
+
+        trip['departure_position'] = departure_position
+        trip['arrival_position'] = arrival_position
+
+
 def emit_trips(day, trips):
     '''
     Print trip details in json to 'trips-<YYYY>-<mm>-<dd>.json'
@@ -170,6 +243,8 @@ def emit_trips(day, trips):
 
 def main():
 
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
     logger.info('Start')
 
     try:
@@ -187,6 +262,9 @@ def main():
 
     # Collect realtime journeys
     trips = get_trips(client, schema, day, interesting_stops)
+
+    # Derive departure and arrival timings
+    derive_timings(trips)
 
     emit_trips(day, trips)
 
